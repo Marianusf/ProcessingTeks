05a7e4d276806ef7ffb176f1284c3dcbe0382e05
Marianusf <marianusjr7@gmail.com>
2025-04-29 15:05:54 +0700

    adding main class for print output and adding item

---
+++ src/TeksIndexing/LinkedListOrderedMain.java
@@ -0,0 +1,14 @@
+package TeksIndexing;
+
+public class LinkedListOrderedMain {
+    public static void main(String[] args) {
+        LinkedListOrdered<String>linkedListSort= new LinkedListOrdered();
+
+        linkedListSort.addSort("lumpia");
+        linkedListSort.addSort("semarang");
+        linkedListSort.addSort("solo");
+        linkedListSort.addSort("rabu");
+
+        System.out.println(linkedListSort.toString());
+    }
+}
